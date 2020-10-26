0fc7b34a2630654ed9ce04aba6080e38f12bd4ef
Dan Webb <dan.webb@damacus.io>
2020-10-26 14:18:55 +0000

    Allow directory and Json file to exist
    - Fixes the tool icon to: https://feathericons.com/?query=edit-3
    - Use -p on mkdir to allow the directory to exist and not fail on edit.
    
    This allows for use in environments where the cache/runner isn't cleared
    every time.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,12 @@
-mkdir ._actionshub_problem-matchers
-cp /yamllint.json ._actionshub_problem-matchers/yamllint.json
-echo "##[add-matcher]._actionshub_problem-matchers/yamllint.json"
+#!/bin/bash
+
+MATCHER_FILE="._actionshub_problem-matchers/yamllint.json"
+
+mkdir -p ._actionshub_problem-matchers
+
+if [[ ! -f ${MATCHER_FILE} ]];then
+    cp /yamllint.json ${MATCHER_FILE}
+fi
+
+echo "##[add-matcher]${MATCHER_FILE}"
 yamllint .
